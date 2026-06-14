df941ba50c8e0e4779fdd0c9072d521d1205f82c
toxicbishop <92860129+toxicbishop@users.noreply.github.com>
2026-06-14 16:44:50 +0530

    Added Sepolia Network Support
    Co-Authored-By: supr1795 <234802632+supr1795@users.noreply.github.com>
    Co-Authored-By: Mohammed0572 <mohammednaqvi725@gmail.com>

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +9,16 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(
+        process.env.MNEMONIC,
+        process.env.SEPOLIA_RPC_URL
+      ),
+      network_id: 11155111,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
     }
   }
 }
