a9f01b852eacab543fb4b39a6bf6ea10c1706402
toxicbishop <pranavarun19@gmail.com>
2026-06-29 23:01:16 +0530

    fix: resolve unused variable errors for build

--- src/pages/Voting.tsx
+++ src/pages/Voting.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { useWeb3 } from '../context/Web3Context';
-import { useLanguage } from '../context/LanguageContext';
 import { Link } from 'react-router-dom';
 import { Chakra } from '../components/Chakra';
 import {
